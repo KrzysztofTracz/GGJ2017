c273a94cc7e875f158f4414285ba988ce151bf02
qzmp <lukasz.golebiowski@mail.ru>
2017-01-21 17:55:41 +0100

    Cam viewport scaling

--- Assets/UIScalingController.cs
+++ Assets/UIScalingController.cs
@@ -9,6 +9,7 @@ public class UIScalingController : MonoBehaviour
 
     public Transform leftPanel, rightPanel, topPanel, botPanel;
     public Canvas uiCanvas;
+    public Camera cam;
 
     private Vector2 referenceResolution;
     private float viewReferenceHeight;
@@ -35,5 +36,12 @@ public class UIScalingController : MonoBehaviour
 
         leftPanel.GetComponent<RectTransform>().sizeDelta = new Vector2(desiredPanelSize, leftPanel.GetComponent<RectTransform>().sizeDelta.y);
         rightPanel.GetComponent<RectTransform>().sizeDelta = new Vector2(desiredPanelSize, rightPanel.GetComponent<RectTransform>().sizeDelta.y);
+
+        float cameraY = botPanel.GetComponent<RectTransform>().rect.height / referenceResolution.y;
+        float cameraHeight = viewReferenceHeight / referenceResolution.y;
+        float cameraWidth = cameraHeight / desiredAspectRatio;
+        float cameraX = (1 - cameraWidth) / 2;
+
+        cam.rect = new Rect(cameraX, cameraY, cameraWidth, cameraHeight);
     }
 }
