1f50fce7dc6f6c0dfee383b189a453b074c2ef57
Konrad Korzec <korzec@gmail.com>
2017-01-22 17:41:55 +0100

    - fix fot start scene

--- Assets/RootManager.cs
+++ Assets/RootManager.cs
@@ -9,37 +9,37 @@ public class RootManager : MonoBehaviour {
 	GameObject startButton;
 	// Use this for initialization
 	void Start () {		
-		try {
-			SceneManager.LoadScene ("scene0_asset_test", LoadSceneMode.Additive);
-		}
-		catch {
-			
-		}
-		try {
-			SceneManager.UnloadScene(SceneManager.GetSceneByName("scene0"));
-		}
-		catch {
-			Debug.Log ("failed to unload scene");
-		}
-		if(ui == null) ui = GameObject.Find ("UI 1");			
-		if(startButton == null) 	startButton = GameObject.Find ("StartButton");
-		ui.SetActive(true);
-		startButton.SetActive(true);
+//		try {
+//			SceneManager.LoadScene ("scene0_asset_test", LoadSceneMode.Additive);
+//		}
+//		catch {
+//			Debug.Log ("Failed to load scene0_asset_test");
+//		}
+//		try {
+//			SceneManager.UnloadScene(SceneManager.GetSceneByName("scene0"));
+//		}
+//		catch {
+//			Debug.Log ("failed to unload scene0");
+//		}
+//		if(ui == null) ui = GameObject.Find ("UI 1");			
+//		if(startButton == null) 	startButton = GameObject.Find ("StartButton");
+//		ui.SetActive(true);
+//		startButton.SetActive(true);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (EntController.Player && EntController.Player.RoundEnded && Input.GetKey ("z")) {
-			EntController.Player.RoundEnded = false;
-			Start ();
-			return;
-		}
+//		if (EntController.Player && EntController.Player.RoundEnded && Input.GetKey ("z")) {
+//			EntController.Player.RoundEnded = false;
+//			Start ();
+//			return;
+//		}
 	}
 
 	public void StartRound() {		
 		GameObject.Find ("UI 1").SetActive(false);
-		SceneManager.LoadScene ("scene0", LoadSceneMode.Additive);
-        SceneManager.LoadScene("scene0_asset_test", LoadSceneMode.Additive);
+		SceneManager.LoadScene("scene0_asset_test", LoadSceneMode.Additive);
+		SceneManager.LoadScene ("scene0", LoadSceneMode.Additive);        
         SceneManager.SetActiveScene(SceneManager.GetSceneByName("scene0"));
 	}
 }
