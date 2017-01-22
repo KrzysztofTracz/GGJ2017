18702f574f409160aab839020d087cb8c80690ad
Konrad Korzec <korzec@gmail.com>
2017-01-22 15:13:37 +0100

    - reset scene on key after its ended

--- Assets/RootManager.cs
+++ Assets/RootManager.cs
@@ -8,7 +8,13 @@ public class RootManager : MonoBehaviour {
 	GameObject ui;
 	GameObject startButton;
 	// Use this for initialization
-	void Start () {
+	void Start () {		
+		try {
+			SceneManager.LoadScene ("scene0_asset_test", LoadSceneMode.Additive);
+		}
+		catch {
+			
+		}
 		try {
 			SceneManager.UnloadScene(SceneManager.GetSceneByName("scene0"));
 		}
@@ -16,21 +22,17 @@ public class RootManager : MonoBehaviour {
 			Debug.Log ("failed to unload scene");
 		}
 		if(ui == null) ui = GameObject.Find ("UI 1 root");			
-		if(startButton == null) startButton = GameObject.Find ("StartButton");
+		if(startButton == null) 	startButton = GameObject.Find ("StartButton");
 		ui.SetActive(true);
 		startButton.SetActive(true);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (EntController.Player && EntController.Player.RoundEnded) {
+		if (EntController.Player && EntController.Player.RoundEnded && Input.GetKey ("z")) {
 			EntController.Player.RoundEnded = false;
 			Start ();
 			return;
-		}
-
-		if (Input.GetKey ("z")) {
-			EntController.Player.RoundEnded = true;
 		}
 	}
 
@@ -40,3 +42,4 @@ public class RootManager : MonoBehaviour {
 		SceneManager.SetActiveScene(SceneManager.GetSceneByName("scene0"));
 	}
 }
+	
